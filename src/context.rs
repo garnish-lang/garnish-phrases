546172c725b3f4df709f5bb6ca87a4b5cdaf297b
chadrc <chadrcollins91@gmail.com>
2024-08-06 08:00:40 -0400

    Added error cases to context

--- src/context.rs
+++ src/context.rs
@@ -1,6 +1,12 @@
 use std::collections::HashMap;
 
 #[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
+pub enum SimpleContextCodes {
+    IncompleteVersionExists,
+    CompleteVersionExists,
+}
+
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq)]
 pub enum PhraseStatus {
     Incomplete,
     Complete,
@@ -20,16 +26,31 @@ impl SimplePhraseContext {
         SimplePhraseContext { part_map: HashMap::new() }
     }
 
-    pub fn add_phrase(&mut self, phrase: &str) -> Result<(), String> {
+    pub fn add_phrase(&mut self, phrase: &str) -> Result<(), SimpleContextCodes> {
         let parts = phrase.split("_").collect::<Vec<&str>>();
 
         for part in parts.iter().take(parts.len() - 1) {
-            self.part_map.insert(part.to_string(), PhraseStatus::Incomplete);
+            match self.part_map.get(*part) {
+                None => {
+                    self.part_map.insert(part.to_string(), PhraseStatus::Incomplete);
+                },
+                Some(status) => if *status == PhraseStatus::Complete {
+                    return Err(SimpleContextCodes::CompleteVersionExists)
+                }
+            }
+
         }
 
         match parts.last() {
             None => unreachable!(),
-            Some(part) => self.part_map.insert(part.to_string(), PhraseStatus::Complete),
+            Some(part) => match self.part_map.get(*part) {
+                None => {
+                    self.part_map.insert(part.to_string(), PhraseStatus::Complete);
+                }
+                Some(status) => if *status == PhraseStatus::Incomplete {
+                    return Err(SimpleContextCodes::IncompleteVersionExists);
+                }
+            }
         };
 
         Ok(())
@@ -47,7 +68,7 @@ impl PhraseContext for SimplePhraseContext {
 
 #[cfg(test)]
 mod tests {
-    use crate::context::{PhraseContext, PhraseStatus, SimplePhraseContext};
+    use crate::context::{PhraseContext, PhraseStatus, SimpleContextCodes, SimplePhraseContext};
 
     #[test]
     fn create() {
@@ -80,5 +101,25 @@ mod tests {
         assert_eq!(result, Ok(()));
         assert_eq!(context.get_phrase_status("some"), PhraseStatus::Incomplete);
         assert_eq!(context.get_phrase_status("phrase"), PhraseStatus::Complete);
+    }
+
+    #[test]
+    fn error_adding_complete_phrase_when_already_incomplete() {
+        let mut context = SimplePhraseContext::new();
+        context.add_phrase("some_phrase").unwrap();
+
+        let result = context.add_phrase("some");
+
+        assert_eq!(result, Err(SimpleContextCodes::IncompleteVersionExists));
+    }
+
+    #[test]
+    fn error_adding_incomplete_phrase_when_already_complete() {
+        let mut context = SimplePhraseContext::new();
+        context.add_phrase("some").unwrap();
+
+        let result = context.add_phrase("some_phrase");
+
+        assert_eq!(result, Err(SimpleContextCodes::CompleteVersionExists));
     }
 }
