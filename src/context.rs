d961742ee5c74dca9e27150da07f3687ecedacf5
chadrc <chadrcollins91@gmail.com>
2024-08-06 07:50:08 -0400

    Initial implementation and tests for context

--- src/context.rs
+++ src/context.rs
@@ -21,7 +21,17 @@ impl SimplePhraseContext {
     }
 
     pub fn add_phrase(&mut self, phrase: &str) -> Result<(), String> {
-        self.part_map.insert(phrase.to_string(), PhraseStatus::Complete);
+        let parts = phrase.split("_").collect::<Vec<&str>>();
+
+        for part in parts.iter().take(parts.len() - 1) {
+            self.part_map.insert(part.to_string(), PhraseStatus::Incomplete);
+        }
+
+        match parts.last() {
+            None => unreachable!(),
+            Some(part) => self.part_map.insert(part.to_string(), PhraseStatus::Complete),
+        };
+
         Ok(())
     }
 }
@@ -32,5 +42,43 @@ impl PhraseContext for SimplePhraseContext {
             None => PhraseStatus::NotAPhrase,
             Some(status) => *status
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::context::{PhraseContext, PhraseStatus, SimplePhraseContext};
+
+    #[test]
+    fn create() {
+        SimplePhraseContext::new();
+    }
+
+    #[test]
+    fn add_single_word_phrase() {
+        let mut context = SimplePhraseContext::new();
+        let result = context.add_phrase("phrase");
+
+        assert_eq!(result, Ok(()));
+        assert_eq!(context.get_phrase_status("phrase"), PhraseStatus::Complete);
+    }
+
+    #[test]
+    fn get_non_phrase() {
+        let mut context = SimplePhraseContext::new();
+        let result = context.add_phrase("phrase");
+
+        assert_eq!(result, Ok(()));
+        assert_eq!(context.get_phrase_status("not"), PhraseStatus::NotAPhrase);
+    }
+
+    #[test]
+    fn add_two_word_phrase() {
+        let mut context = SimplePhraseContext::new();
+        let result = context.add_phrase("some_phrase");
+
+        assert_eq!(result, Ok(()));
+        assert_eq!(context.get_phrase_status("some"), PhraseStatus::Incomplete);
+        assert_eq!(context.get_phrase_status("phrase"), PhraseStatus::Complete);
     }
 }
